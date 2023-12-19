170885385d343daf759f34bfbdf4d30e13eecae2
Aadarsh A <aadarsh.ram@gmail.com>
2023-12-19 09:56:17 +0530

    feat: Chat QA with LLM (#15)
    * feat: LLM Chat with memory
    
    * fix: Remove nested chat history
    
    * fix: Update requirements.txt

--- app.py
+++ app.py
@@ -4,12 +4,22 @@ import json
 import sys
 
 from dotenv import load_dotenv
-from llm_model_inference import summarize_pdf, grammar_check
+from llm_model_inference import summarize_pdf, grammar_check, ingest_file, chat_qa
 
 load_dotenv()
 MQ_SEND_QUEUE = os.getenv('MQ_SEND_QUEUE')
 MQ_RECV_QUEUE = os.getenv('MQ_RECV_QUEUE')
 QUEUE_HEARTBEAT = os.getenv('QUEUE_HEARTBEAT')
+
+def check_errors(result, result_json):
+    """Change result JSON according to success/failure"""
+    if isinstance(result, Exception):
+        result_json['status'] = 'fail'
+        result_json['content'] = result.args[0]
+    else:
+        result_json['status'] = 'success'
+        result_json['content'] = result
+    return result_json
 
 def main():
     connection = pika.BlockingConnection(pika.ConnectionParameters('localhost', heartbeat=int(QUEUE_HEARTBEAT)))
@@ -24,24 +34,25 @@ def main():
 
         if client_json['type'] == 'summary':
             result = summarize_pdf(client_json['content'])
-            if isinstance(result, Exception):
-                result_json['status'] = 'fail'
-                result_json['content'] = result.args[0]
-            else:
-                result_json['status'] = 'success'
-                result_json['content'] = result
-            
+            result_json = check_errors(result, result_json)
             print ('[*] Sent summary!')
         
         elif client_json['type'] == 'grammar':
             result = grammar_check(client_json['content'])
-            if isinstance(result, Exception):
-                result_json['status'] = 'fail'
-                result_json['content'] = result.args[0]
-            else:
-                result_json['status'] = 'success'
-                result_json['content'] = result
+            result_json = check_errors(result, result_json)
             print ('[*] Sent corrected text!')
+        
+        elif client_json['type'] == 'upload':
+            result = ingest_file(client_json['content'])
+            result_json = check_errors(result, result_json)
+            print ('[*] Uploaded successfully!')
+
+        elif client_json['type'] == 'chat':
+            query = client_json['query']
+            chat_history = client_json['chat_history']
+            result = chat_qa(query, chat_history)
+            result_json = check_errors(result, result_json)
+            print ('[*] Sent reply!')
         
         channel.basic_publish(exchange='', routing_key=MQ_SEND_QUEUE, body=json.dumps(result_json))
 
--- send_test.py
+++ send_test.py
@@ -8,21 +8,53 @@ channel.queue_declare(queue='gpt-response', durable=True)
 channel.queue_declare(queue='gpt-send', durable=True)
 
 # Summary test
-# send_summary_json = {
-#     'type' : 'summary',
-#     'content' : '/home/aadarsh/src/llm-sih-test/my-gpt-backend/samples/trump-article.pdf'
-# }
+def summary_test():
+    send_summary_json = {
+        'type' : 'summary',
+        'content' : '/home/aadarsh/src/llm-sih-test/my-gpt-backend/samples/trump-article.pdf'
+    }
 
-# channel.basic_publish(exchange='', routing_key='gpt-send', body=json.dumps(send_summary_json))
-# print("[x] Sent PDF")
+    channel.basic_publish(exchange='', routing_key='gpt-send', body=json.dumps(send_summary_json))
+    print("[x] Sent PDF")
 
 # Grammar test
-send_grammar_json = {
-    'type' : 'grammar',
-    'content' : "did you no that bats are mammals. we no they are mammals just lik us becaus they are warm blooded they are the only mammals that no how to fly bats are Nocturnal which means thay sleep during the day and are awak at nite?."
-}
-channel.basic_publish(exchange='', routing_key='gpt-send', body=json.dumps(send_grammar_json))
-print("[x] Sent wrong text")
+def grammar_test():    
+    send_grammar_json = {
+        'type' : 'grammar',
+        'content' : "did you no that bats are mammals. we no they are mammals just lik us becaus they are warm blooded they are the only mammals that no how to fly bats are Nocturnal which means thay sleep during the day and are awak at nite?."
+    }
+    channel.basic_publish(exchange='', routing_key='gpt-send', body=json.dumps(send_grammar_json))
+    print("[x] Sent wrong text")
+
+# Upload test
+def upload_test():
+    send_upload_json = {
+        'type' : 'upload',
+        'content' : '/home/aadarsh/src/llm-sih-test/my-gpt-backend/samples/gaza-article.pdf'
+    }
+    channel.basic_publish(exchange='', routing_key='gpt-send', body=json.dumps(send_upload_json))
+    print ("[x] Sent upload file")
+
+# Chat test
+def chat_test():
+    send_chat_json1 = {
+        'type' : 'chat',
+        'query' : 'How many captives had Hamas freed?',
+        'chat_history' : []
+    }
+    channel.basic_publish(exchange='', routing_key='gpt-send', body=json.dumps(send_chat_json1))
+
+    send_chat_json2 = {
+        'type' : 'chat',
+        'query' : 'Among those, how many were US citizens?',
+        'chat_history' : [
+            'How many captives had Hamas freed?', " According to the information provided, Hamas released four captives, including two US citizens, as a gesture of goodwill towards Israel\'s demand for a prisoner swap deal that includes the release of all Palestinian prisoners from Israeli jails in exchange for all hostages held in Gaza."
+        ]
+    }
+    channel.basic_publish(exchange='', routing_key='gpt-send', body=json.dumps(send_chat_json2))
+
+# Sample invocation
+grammar_test()
 
 def callback(ch, method, properties, body):
     print (f"Received {body}")
